88187e86a3922c2e65fe4376acaf4843f9e28cf9
Volodymyr Zadorozhnii <volodymyr.zadorozhnii@brightechstudio.com>
2023-07-26 23:46:33 +0300

    connect favorite button to header

--- src/components/weatherSearch.vue
+++ src/components/weatherSearch.vue
@@ -30,9 +30,9 @@ import IconSearch from './icons/IconSearch.vue'
         </li>
       </ul>
     </div>
-    <!-- <span > -->
-      <iconFavorite :favorite="this.isFavorite" @click="addToFavorite" class="favorite" />
-    <!-- </span> -->
+    <span @click="addToFavorite" class="favorite">
+      <iconFavorite  />
+    </span>
   </div>
 </template>
 
